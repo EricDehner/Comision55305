d7fb1661b550f41016d8b0ae8c6fa4897d6aec0a
Eric Dehner <eric.dehner.ga@gmail.com>
2023-10-18 22:23:04 -0300

    Entrega Integradora 2

---
+++ Entrega Integradora 2/src/public/js/login.js
@@ -0,0 +1,48 @@
+const loginUser = async () => {
+    let email = document.getElementById("email").value;
+    let password = document.getElementById("password").value;
+
+    try {
+        const response = await fetch("/api/sessions/login/", {
+            method: "POST",
+            headers: { "Content-type": "application/json; charset=UTF-8" },
+            body: JSON.stringify({ email: email, password: password }),
+        });
+
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+
+        const data = await response.json();
+        if (data.status === "success") {
+            window.location.href = "/products";
+        }
+    } catch (error) {
+        Toastify({
+            text: "No se pudo loguear el Usuario!",
+            duration: 1500,
+            position: "right",
+            offset: {
+                x: 0,
+                y: 55,
+            }
+        }).showToast();
+
+        console.log("Hubo un problema con la operación, usuario o contraseña incorrectos", error);
+    }
+};
+
+
+const passwordInput = document.getElementById('password');
+const togglePassword = document.getElementById('togglePassword');
+const visibilityIcon = document.getElementById('visibilityIcon');
+
+togglePassword.addEventListener('click', function () {
+    if (passwordInput.type === 'password') {
+        passwordInput.type = 'text';
+        visibilityIcon.textContent = 'visibility_off';
+    } else {
+        passwordInput.type = 'password';
+        visibilityIcon.textContent = 'visibility';
+    }
+});
---
+++ Entrega Integradora 2/src/public/js/register.js
@@ -0,0 +1,93 @@
+const registerUser = async () => {
+    let first_name = document.getElementById("first_name").value;
+    let last_name = document.getElementById("last_name").value;
+    let email = document.getElementById("email").value;
+    let age = document.getElementById("age").value;
+    let password = document.getElementById("password").value;
+
+    const user = { first_name, last_name, email, age, password };
+
+  try {
+    const response = await fetch("/api/sessions/register", {
+      method: "POST",
+      headers: { "Content-type": "application/json; charset=UTF-8" },
+      body: JSON.stringify(user),
+    });
+
+    if (!response.ok) {
+        console.error("Error al registrar el usuario:", await response.text());
+    } else {
+        const data = await response.json();
+        if (data.status === "success" && data.redirect) {
+            window.location.href = data.redirect;
+        }
+    }
+  } catch (error) {
+    console.error("Hubo un error al registrar el usuario:", error);
+  }
+
+
+
+
+
+
+    /* if (first_name !== "" && last_name !== "" && email !== "" && age !== "" && password !== "") {
+        if (/^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/i.test(email)) {
+            const user = { first_name, last_name, email, age, password };
+
+            const response = await fetch("/api/sessions/register", {
+                method: "POST",
+                headers: { "Content-type": "application/json; charset=UTF-8" },
+                body: JSON.stringify(user),
+            });
+            const data = await response.json();
+
+            Toastify({
+                text: "¡Usuario registrado!",
+                duration: 1500,
+                position: "right",
+                offset: {
+                    x: 0,
+                    y: 55,
+                }
+            }).showToast();
+            setTimeout(() => {
+                location.href = "/login";
+            }, 1500);
+        } else {
+            Toastify({
+                text: "¡Por favor ingrese una dirección de correo electrónico válida!",
+                duration: 1500,
+                position: "right",
+                offset: {
+                    x: 0,
+                    y: 55,
+                }
+            }).showToast();
+        }
+    } else {
+        Toastify({
+            text: "¡Por favor complete todos los campos!",
+            duration: 1500,
+            position: "right",
+            offset: {
+                x: 0,
+                y: 55,
+            }
+        }).showToast();
+    } */
+}
+
+const passwordInput = document.getElementById('password');
+const togglePassword = document.getElementById('togglePassword');
+const visibilityIcon = document.getElementById('visibilityIcon');
+
+togglePassword.addEventListener('click', function () {
+    if (passwordInput.type === 'password') {
+        passwordInput.type = 'text';
+        visibilityIcon.textContent = 'visibility_off';
+    } else {
+        passwordInput.type = 'password';
+        visibilityIcon.textContent = 'visibility';
+    }
+});
